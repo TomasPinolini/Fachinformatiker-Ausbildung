f0d9d3a560963380a8cc467496028681766b797f
Pinolini Tomas (LoP1/TEF7) <pit5lo@bosch.com>
2025-01-20 08:17:34 +0100

    08:17 - 20.01.2025 - pre Frühstuck

--- NachDurchlaufplan/3-WBnDataHandling/1-WB/2/nodejsCrash/index.js
+++
@@ -1,7 +0,0 @@
-import {getPosts} from '.postcontroller.js' ;
-console.log(getPosts());
-
-const {grandom, ctof} = require('./utils');
-
-// console.log(`Random Number: ${generateRandomNumber()}`);
-// console.log(`Celcius to fahrenheit: ${ctof(0)}`);
